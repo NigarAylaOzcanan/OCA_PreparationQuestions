e99200d1070647ba7ea4cee13cc00168236b48e1
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-08-21 14:59:17 +0300

    initial commit

---
+++ src/oca_2022/q185p3/C.java
@@ -0,0 +1,13 @@
+package oca_2022.q185p3;
+//line n2
+import oca_2022.q185.A;
+import oca_2022.q185.p2.B;
+
+
+public class C {
+    public static void main(String[] args) {
+        A o1= new A();
+        B o2= new B();
+    }
+}
+
